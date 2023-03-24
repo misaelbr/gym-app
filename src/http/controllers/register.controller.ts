a443861165498b7025ec12b060105c2e67482c1d
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2023-03-24 17:32:57 -0300

    Fix: fixed error in controller

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -17,7 +18,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerUseCase.execute({ name, email, password })
   } catch (error) {
-    if (error instanceof Error) {
+    if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message })
     }
 
