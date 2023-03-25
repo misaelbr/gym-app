4fc44f833f2aa50c18999029e9f7e7b57db954b1
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2023-03-25 00:18:35 -0300

    feat: create basic suport to able check in the gym

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,110 @@
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './check-in'
+import { InMemoryGimsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { Decimal } from '@prisma/client/runtime/library'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let gymsRepository: InMemoryGimsRepository
+let checkInUseCase: CheckInUseCase
+
+describe('Check In Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    gymsRepository = new InMemoryGimsRepository()
+    checkInUseCase = new CheckInUseCase(checkInsRepository, gymsRepository)
+
+    gymsRepository.items.push({
+      id: '456',
+      title: 'Javascript GYm',
+      description: 'A gym for javascript developers',
+      latitude: new Decimal(-29.720723),
+      longitude: new Decimal(-52.5211914),
+      created_at: new Date(),
+      updated_at: null,
+      phone: null,
+    })
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await checkInUseCase.execute({
+      userId: '343',
+      gymId: '456',
+      userLatitude: -29.720723,
+      userLongitude: -52.5211914,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should be not able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+    // -29.720723,-52.5211914
+    await checkInUseCase.execute({
+      userId: '123',
+      gymId: '456',
+      userLatitude: -29.720723,
+      userLongitude: -52.5211914,
+    })
+
+    await expect(() =>
+      checkInUseCase.execute({
+        userId: '123',
+        gymId: '456',
+        userLatitude: -29.720723,
+        userLongitude: -52.5211914,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in twice in the different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await checkInUseCase.execute({
+      userId: '123',
+      gymId: '456',
+      userLatitude: -29.720723,
+      userLongitude: -52.5211914,
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await checkInUseCase.execute({
+      userId: '123',
+      gymId: '456',
+      userLatitude: -29.720723,
+      userLongitude: -52.5211914,
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    gymsRepository.items.push({
+      id: '457',
+      title: 'Javascript GYm',
+      description: 'A gym for javascript developers',
+      latitude: new Decimal(-29.7198873),
+      longitude: new Decimal(-52.503749),
+      created_at: new Date(),
+      updated_at: null,
+      phone: null,
+    })
+
+    await expect(() =>
+      checkInUseCase.execute({
+        userId: '123',
+        gymId: '457',
+        userLatitude: -29.720723,
+        userLongitude: -52.5211914,
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
