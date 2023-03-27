4b9b65cc657ba3b45f5adee3e182bd12ec264e8b
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2023-03-27 14:52:58 -0300

    test: add coverage to branch in tests

--- src/use-cases/create-gym.spec.ts
+++ src/use-cases/create-gym.spec.ts
@@ -14,12 +14,40 @@ describe('Create Gym Use Case', () => {
   it('shoud be able to create Gym', async () => {
     const { gym } = await sut.execute({
       title: 'JavaScript Gym',
-      description: 'A gym for javascript developers',
+      description: null,
       phone: null,
       latitude: -29.7198873,
       longitude: -52.503749,
     })
 
     expect(gym.id).toEqual(expect.any(String))
+  })
+
+  it('should be able to find a Gym by id', async () => {
+    const { gym } = await sut.execute({
+      title: 'JavaScript Gym',
+      description: 'A gym for javascript developers',
+      phone: null,
+      latitude: -29.7198873,
+      longitude: -52.503749,
+    })
+
+    const gymFound = await inMemoryGymnsRepository.findById(gym.id)
+
+    expect(gymFound).toEqual(gym)
+  })
+
+  it('should be not able to find a Gym by invalid id', async () => {
+    await sut.execute({
+      title: 'JavaScript Gym',
+      description: 'A gym for javascript developers',
+      phone: null,
+      latitude: -29.7198873,
+      longitude: -52.503749,
+    })
+
+    const gymFound = await inMemoryGymnsRepository.findById('invalid id')
+
+    expect(gymFound).toEqual(null)
   })
 })
